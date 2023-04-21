3da9107c8e8687b6278660f31fe1af7877ed871c
Овнер Алексей <alexey.sinotov@gmail.com>
2023-04-21 11:55:05 +0300

    Alert + title

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
-/* Это объявление переменной, мы наши кнопку по тегу */
-const button = document.querySelector('button');
+// Получаем кнопку по ID
+const button = document.getElementById('show-alert');
 
-/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener('click', function() {
-	alert('Берите в команду ;)')
-})
+// Навешиваем обработчик события на клик по кнопке
+button.addEventListener('click', () => {
+  // Показываем alert с помощью SweetAlert
+  swal({
+    title: 'Берите в команду',
+//  text: 'Текст alert-а', 
+    icon: 'success',
+    buttons: {
+      confirm: {
+        text: 'OK',
+        className: 'button'
+      }
+    },
+    // Добавляем картинку в alert
+    content: {
+      element: 'img',
+      attributes: {
+        src: 'emoji.png',
+        style: 'max-width: 100%'
+      }
+    }
+  });
+});
